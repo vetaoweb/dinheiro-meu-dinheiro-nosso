d838f62ff7ad45a89f63b898c9cbb0b6aeb83cc3
vetaoweb <vetao2@gmail.com>
2026-06-26 09:54:59 -0300

    feat: adicionar service worker

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'dmdn-shell-v1';
+const SHELL = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/cadastro.html',
+  '/css/tokens.css',
+  '/css/global.css',
+  '/css/marketing.css',
+  '/css/auth.css',
+  '/css/app.css',
+  '/assets/logo.svg',
+  '/assets/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.hostname.includes('supabase.co') || event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
