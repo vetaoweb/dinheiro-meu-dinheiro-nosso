c64f1333ce808d7d05c6e4d6a8a7fa36da2071c8
vetaoweb <vetao2@gmail.com>
2026-06-26 10:34:17 -0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,6 +1,6 @@
 window.DMDN_ENV = Object.freeze({
   SUPABASE_URL: 'https://bcsctqfelxlzwcskgmmp.supabase.co',
-  SUPABASE_ANON_KEY: '__INFORME_A_CHAVE_ANON_PUBLICA__',
+  SUPABASE_ANON_KEY: 'sb_publishable_M1i22_GaSsmQrKtWrtXkEQ_zA90T-bS',
   APP_URL: window.location.origin,
   APP_NAME: 'Dinheiro Meu é Dinheiro Nosso!'
 });
